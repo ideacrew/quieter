116ef886777df26fca0965dd20f2dda3bf03bbec
Marco (Polo) Ornelas <marco.ornelas@ideacrew.com>
2024-06-07 11:59:08 -0400

    added styles, using bulma

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'aha/explorer', as: 'aha_explorer'
+  get 'aha/initiative_explorer', as: 'execute_aha_initiative_explorer'
+
   get 'reports/home' 
   get 'reports/range', as: 'range_report'
   post 'reports/execute_range', as: 'execute_range_report'
